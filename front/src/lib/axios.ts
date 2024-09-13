c39d0c731867072ce73706787519fc2ff1a5c1b6
JiwanBang <jiwanbang@gmail.com>
2024-09-13 13:54:52 +0900

    fix: axios baseURL

--- front/src/lib/axios.ts
+++ front/src/lib/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "/api",
+  baseURL:
+    "http://ec2-43-203-251-58.ap-northeast-2.compute.amazonaws.com:3080/api",
   // withCredentials: true,
 });
 
