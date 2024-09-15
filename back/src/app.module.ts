eb6834f23808e2ce587aae765e8ee54792f9842b
JiwanBang <jiwanbang@gmail.com>
2024-09-15 13:28:36 +0900

    fix: env path to __dirname

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -15,7 +15,7 @@ import { PicturesModule } from './pictures/pictures.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '/var/www/backend/.env',
+      envFilePath: `${__dirname}/../.env`,
     }),
     TypeOrmModule.forRootAsync({
       // forRoot -> forRootAsync로 변경
